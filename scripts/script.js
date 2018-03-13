913e041327f470b1dc7f09cf6236672dad5bb381
Neil Keddie <neil.keddie@btinternet.com>
2018-03-13 18:46:33 +0000

    Squad pictures added, team files updated, social link page created, header links updated

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,39 @@ function myMap() {
   };
   var map = new google.maps.Map(mapCanvas, mapOptions);
 }
+
+$('#contact-form').bootstrapValidator({
+//        live: 'disabled',
+        message: 'This value is not valid',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            Name: {
+                validators: {
+                    notEmpty: {
+                        message: 'The Name is required and cannot be empty'
+                    }
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'The email address is required'
+                    },
+                    emailAddress: {
+                        message: 'The email address is not valid'
+                    }
+                }
+            },
+            Message: {
+                validators: {
+                    notEmpty: {
+                        message: 'The Message is required and cannot be empty'
+                    }
+                }
+            }
+        }
+    });
