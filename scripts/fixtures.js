6dd88887bac653273676bff322c9a0fb0fd24512
Neil Keddie <neil.keddie@btinternet.com>
2018-04-04 12:25:13 +0000

    Add fixture functionality on marquee and fixtures page, removed fixtures on footer

---
+++ scripts/fixtures.js
@@ -0,0 +1,103 @@
+queue()
+    .defer(d3.csv, "data/HockeyFixtures.csv")
+    .await(showFixtures);
+    
+	var globalFixtures = [];
+	var teamName;
+
+function showFixtures(error, team) {
+    
+    var ndx = crossfilter(team);
+	show_fixtures(ndx, "All");
+	document.getElementById("all").onclick = function() {$("#fixtures").empty();show_fixtures(ndx, "All")};
+	document.getElementById("kubz").onclick = function() {$("#fixtures").empty();show_fixtures(ndx, "KUBZ")};
+	document.getElementById("eagles").onclick = function() {$("#fixtures").empty();show_fixtures(ndx, "EAGLES")};
+	document.getElementById("redskins").onclick = function() {$("#fixtures").empty();show_fixtures(ndx, "REDSKINS")};
+	document.getElementById("chiefs").onclick = function() {$("#fixtures").empty();show_fixtures(ndx, "CHIEFS")};
+	document.getElementById("flames").onclick = function() {$("#fixtures").empty();show_fixtures(ndx, "FLAMES")};
+	document.getElementById("falcons").onclick = function() {$("#fixtures").empty();show_fixtures(ndx, "FALCONS")};
+	document.getElementById("kestrels").onclick = function() {$("#fixtures").empty();show_fixtures(ndx, "KESTRELS")};
+    dc.renderAll();
+}
+
+function findObjectByKey(array, key, value) {
+	
+    for (i = 0; i < array.length; i++) {
+        if (array[i][key] === value){
+            globalFixtures.push(array[i]);
+        }
+    }
+}
+
+function show_fixtures(ndx, team) {
+	
+	teamName = team;
+	
+     d3.csv("data/HockeyFixtures.csv", function(error, data) {
+		  if (error) throw error;
+		  
+		  var table = d3.select('#fixtures').append('table');
+		  var titles = d3.keys(data[0]);
+		  var headers = table.append('thead').append('tr')
+		                   .selectAll('th')
+		                   .data(titles).enter()
+		                   .append('th')
+		                   .text(function (d) {
+			                    return d;
+		                    });
+		  if (teamName === '' || teamName === 'All') {
+		  			  var rows = table.append('tbody').selectAll('tr')
+		               .data(data)
+		               .enter()
+		               .append('tr');
+		               
+		 
+		  rows.selectAll('td')
+		  .data(function (d) {
+		   	return titles.map(function (k) {
+		   		return { 'value': d[k], 'name': k};
+		    	});
+		    })
+		    .enter()
+		    .append('td')
+		    .attr('data-th', function (d) {
+		    	return d.name;
+		    })
+		    .text(function (d) {
+		    	return d.value;
+		    });
+		  }	else {
+		  globalFixtures = [];
+		  findObjectByKey(data, 'team', teamName);
+		  	
+		  console.log(teamName);
+		  var rows = table.append('tbody').selectAll('tr')
+		               .data(globalFixtures)
+		               .enter()
+		               .append('tr');
+		               
+		 
+		  rows.selectAll('td')
+		  .data(function (d) {
+		   	return titles.map(function (k) {
+		   		return { 'value': d[k], 'name': k};
+		    	});
+		    })
+		    .enter()
+		    .append('td')
+		    .attr('data-th', function (d) {
+		    	return d.name;
+		    })
+		    .text(function (d) {
+		    	return d.value;
+		    });
+
+		  }
+	$("th").css("text-transform","uppercase");
+	$("th").css("background-color","#ffff4d");
+	$("tr:odd>td").css("background-color","#14326a");
+	$("tr:odd>td").css("color","white");
+    $("tr:even>td").css("background-color","white",);
+    $("tr:even>td").css("color","#14326a");
+	  });
+}
---
+++ scripts/upcomingfixtures.js
@@ -0,0 +1,52 @@
+queue()
+    .defer(d3.csv, "data/HockeyFixtures.csv")
+    .await(upcomingFixtures);
+   
+    var nextFixtures = [];
+    
+    var today = new Date();
+    var secsToday = today.getTime();
+    var todayPlus = new Date();
+        todayPlus.setDate(todayPlus.getDate() + 6);
+    var secsTodayPlus = todayPlus.getTime();
+    
+    console.log(secsToday);
+    console.log(secsTodayPlus);
+
+function upcomingFixtures(error, date) {
+
+    var ndx = crossfilter(date);
+    find_fixtures(ndx, today);
+    dc.renderAll();
+}
+
+function findObjectByKey(array, key, date, datePlus) {
+	
+    for (i = 0; i < array.length; i++) {
+        var newDate = array[i][key].split("/").reverse().join("-");
+        var secsNewDate = (Date.parse(newDate));
+        
+        
+        if ((date <= secsNewDate) && (datePlus >= secsNewDate)){
+            console.log(array[i][key]);
+            nextFixtures.push(array[i]);
+        }
+    }
+}
+
+function find_fixtures(ndx, date) {
+
+    d3.csv("data/HockeyFixtures.csv", function(error, data) {
+		          
+		  findObjectByKey(data, 'date', secsToday, secsTodayPlus);
+		  console.log(nextFixtures);
+		  var marqueeText = 'This weeks fixtures: ';
+		  for (i=0;i < nextFixtures.length;i++) {
+		  console.log(nextFixtures[i].team);
+		  marqueeText += ('  '+nextFixtures[i].date+' '+nextFixtures[i].team+' v '+nextFixtures[i].opponents+' '+nextFixtures[i].where+' '+nextFixtures[i].time+'        ');
+		                                                    
+		  }
+		  document.getElementById('marquee').innerHTML += ('<p>'+marqueeText+'</p>');            
+		 
+    });
+}
