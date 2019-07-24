a44f91c9951afba91357a5c0d494e355650cd67d
neilkeddie <neil.keddie@btinternet.com>
2019-07-24 21:58:37 +0100

    neilkeddie | Change KIHC website to Ruby app - initial commit

---
+++ app.rb
@@ -0,0 +1,81 @@
+require 'sinatra'
+
+get '/' do
+    erb :index
+end
+
+get '/home' do
+    erb :home
+end
+
+get '/about' do
+    erb :about
+end
+
+get '/awards' do
+    erb :awards
+end
+
+get '/committee' do
+    erb :committee
+end
+
+get '/enquiry' do
+    erb :enquiry
+end
+
+get '/fixtures' do
+    erb :fixtures
+end
+
+get '/fixtures-201819' do
+    erb :fixtures201819
+end
+
+get '/hockeyuk' do
+    erb :hockeyuk
+end
+
+get '/members' do
+    erb :members
+end
+
+get '/social' do
+    erb :social
+end
+
+get '/sponsors' do
+    erb :sponsors
+end
+
+get '/where' do
+    erb :where
+end
+
+get '/chiefs' do
+    erb :'teams/chiefs'
+end
+
+get '/eagles' do
+    erb :'teams/eagles'
+end
+
+get '/falcons' do
+    erb :'teams/falcons'
+end
+
+get '/flames' do
+    erb :'teams/flames'
+end
+
+get '/kestrels' do
+    erb :'teams/kestrels'
+end
+
+get '/kubz' do
+    erb :'teams/kubz'
+end
+
+get '/redskins' do
+    erb :'teams/redskins'
+end
